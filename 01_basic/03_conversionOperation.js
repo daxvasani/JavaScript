44dad8bb15cbbce1d0ef4dbdcd8af0d3f7110a8e
daxvasani <daxvasani209@gmail.com>
2025-06-16 18:14:16 +0000

    Comparsion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,32 +1,54 @@
-let score = 33
-let scoree = "33"
-let scoreee = "33abc"
+// let score = 33
+// let scoree = "33"
+// let scoreee = "33abc"
 
-console.log(typeof score);
-console.log(typeof scoree);
+// console.log(typeof score);
+// console.log(typeof scoree);
 
-let valueInNumber = Number(scoree)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(scoree)
+// console.log(typeof valueInNumber);
 
-let valueInNumberr = Number(scoreee)
-console.log(typeof valueInNumberr);
-console.log(valueInNumberr);
+// let valueInNumberr = Number(scoreee)
+// console.log(typeof valueInNumberr);
+// console.log(valueInNumberr);
 
-// if null than outpt 0 and if undefined than output NaN
-// if boolean true than output 1 and false than output 0
-// if string which cannot be converted than output NaN
+// // if null than outpt 0 and if undefined than output NaN
+// // if boolean true than output 1 and false than output 0
+// // if string which cannot be converted than output NaN
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-//1 => true; 0=> false
-// if we "" (empty) than output is False
-// if we string than output is True
+// //1 => true; 0=> false
+// // if we "" (empty) than output is False
+// // if we string than output is True
 
-let somenumber = 22
+// let somenumber = 22
 
-let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// let stringnumber = String(somenumber)
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
 
+// // ********** Operation ***************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);
+console.log(+true);
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
