d6b3fa90acb11a215b5254b6107144695f02738d
daxvasani <daxvasani209@gmail.com>
2025-06-17 07:47:26 +0000

    Strings

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -1,1 +1,18 @@
-console.log('2'+2-'2');
+// strings can be denoted in double qout as well
+// as single qout ( '', "")
+
+"hello" + "world"
+'hello world'
+
+const name = "hello "
+const name1 = "user! "
+console.log(name+name1+"Welcome back!"); // too outdated
+
+const sen1 = "Dax"
+const sen2 = "Vasani"
+console.log(`Hello my name is ${sen1} and my surename is ${sen2}.`);
+
+const gameName = new String('GTA V') // Another way of declaring a string
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
