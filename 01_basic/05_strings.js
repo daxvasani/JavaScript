e8c45ad7b87b6eeb57158e916cc5f4f7a899886f
daxvasani <daxvasani209@gmail.com>
2025-06-18 05:48:11 +0000

    Strings in detail and nums

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -1,18 +1,38 @@
-// strings can be denoted in double qout as well
-// as single qout ( '', "")
+// // strings can be denoted in double qout as well
+// // as single qout ( '', "")
 
-"hello" + "world"
-'hello world'
+// "hello" + "world"
+// 'hello world'
 
-const name = "hello "
-const name1 = "user! "
-console.log(name+name1+"Welcome back!"); // too outdated
+// const name = "hello "
+// const name1 = "user! "
+// console.log(name+name1+"Welcome back!"); // too outdated
 
-const sen1 = "Dax"
-const sen2 = "Vasani"
-console.log(`Hello my name is ${sen1} and my surename is ${sen2}.`);
+// const sen1 = "Dax"
+// const sen2 = "Vasani"
+// console.log(`Hello my name is ${sen1} and my surename is ${sen2}.`);
 
-const gameName = new String('GTA V') // Another way of declaring a string
+const gameName = new String('gta-V-pc-game') // Another way of declaring a string
 console.log(gameName[0])
 console.log(gameName.__proto__);
 
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const anotherString = gameName.slice(-1,0) // doubt
+console.log(anotherString);
+
+const newStringOne = '   dax    '
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://googl%20e.com"
+console.log(url.replace('%20',''));
+console.log(url.includes('com'));
+
+console.log(gameName.split('-'));
---
+++ 01_basic/06_nums_math.js
@@ -0,0 +1,10 @@
+// Introduction to Nums and maths in JS
+
+const score = 400
+console.log(score);
+
+const balance = new  Number(100)
+console.log(balance);
+//console.log(balance.toString); // converted to string
+console.log(balance.toString().length);  
+console.log(balance.toFixed(3)); // after dot numbers
