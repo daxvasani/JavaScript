03658aa8c52330c664cc681b4a1e72c97485b751
daxvasani <daxvasani209@gmail.com>
2025-06-18 16:15:01 +0000

    Dates in detail

--- 01_basic/07_datesinJS.js
+++ 01_basic/07_datesinJS.js
@@ -11,3 +11,19 @@ console.log(myCreatedDate.toDateString()); // start with 0(jan) in jS
 
 let myCreatedDateTime = new Date(2023,2,20,6,12,58) // declaration with time
 console.log(myCreatedDateTime.toLocaleString());
+
+let myCreatedSpecificDate = new Date("2025-06-18") // gives dates in specific manner (dd-mm-yy)
+console.log(myCreatedSpecificDate.toLocaleString()); // month start with 01
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp); // this gives output in miliseconds
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000)); // converted into seconds
+
+let newDate = new Date()
+console.log(newDate); // get new random date
+console.log(newDate.getDay()); // gives specific day or specific month from the certain date
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+}) // define an object and can give many parameters
