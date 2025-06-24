364f190985fbdd3ac6a0f3a9ada78bb4bc1aa75e
daxvasani <daxvasani209@gmail.com>
2025-06-24 17:01:56 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -15,7 +15,10 @@ sayMyName() // exclusion
 sayMyName()
 
 function addTwoNumbers(number1, number2){ // number1, number2 are called parameters
-    console.log(number1+number2)
+    //two different ways..
+    // let result = number1+number2
+    // return result
+    return number1+number2
 } 
 
 addTwoNumbers() // agrument-less Output: NaN
@@ -25,3 +28,45 @@ addTwoNumbers(1,"2") // Output: 12
 
 const result = addTwoNumbers(5,5)
 console.log("Result: ",result);
+
+function loginUserMessage(username){ // if you want to pass default value than just write: username = "dax" than if block will never excute.
+
+    if(username === undefined){
+        console.log("Please enter a username")
+    }
+    return `${username} just logged in!`
+}
+console.log(loginUserMessage("Dax"))
+// if you do not pass any value it will return undefined.
+console.log(loginUserMessage()) // here is the example of undefined.
+
+function calculateCartPrice(val1, ...num1){ // ...  is called rest operator as well as spread operator.
+    return num1
+} // here ... is rest operator which merge elements into an array
+// val1 carries 200 from the array, and other are printed
+console.log(calculateCartPrice(200,400,500))
+
+const user = {
+    username: "daxvasani",
+    age: 19
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and age is ${anyObject.age}`)
+}
+
+handleObject(user)
+// it is not complusory to create object and than pass it.
+
+handleObject({          // object creation
+    username: "Dax",
+    age: 19
+})
+
+const myNewArray = [200,400,600,800]
+
+function returnSecondValue(getArray){
+    return getArray[0]
+}
+console.log(returnSecondValue(myNewArray));
+// Same as object, We have seen array also
